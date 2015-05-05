8482c589e4b105d5aa1198c10d0f5e4905d37e4d
larik <larensolarsuss@gmail.com>
2015-05-05 17:55:21 +0600

    angular dir start

--- subutai-app/subutai-app.js
+++ subutai-app/subutai-app.js
@@ -1,1 +1,25 @@
-var app = angular.module("subutai-app", []);
+var app = angular.module("subutai-app", [
+    'ui.router',
+    //'pascalprecht.translate',
+    //'app.notifyGrowl'
+])
+    .config(routesConf)
+
+routesConf.$inject = ['$stateProvider'];
+
+function routesConf($stateProvider) {
+
+    $stateProvider
+    .state(
+        "home", {
+            url: "/home",
+            templateUrl: "subutai-app/home/partials/view.html"
+        }
+    )
+    .state(
+        "console", {
+            url: "/console",
+            templateUrl: "subutai-app/console/partials/view.html"
+        }
+    );
+}
