d5a3ec7d2ea7084e5346038b28fd14a8994be3aa
aziza <akubatbekk@safehaus.org>
2015-05-08 12:25:27 +0600

    identity controller fixed

--- subutai-app/identity/controller.js
+++ subutai-app/identity/controller.js
@@ -4,9 +4,10 @@
 angular.module('subutai.identity.controller',[])
     .controller('IdentityTableCtrl', IdentityTableCtrl);
 
-IdentityTableCtrl.$inject = ['$scope'];
-function IdentityTableCtrl($scope) {
-    $http.get("users.json").success(function (res) {
-        $scope.users = res;
+IdentityTableCtrl.$inject = ['$scope', '$http'];
+function IdentityTableCtrl($scope, $http) {
+    $http.get("subutai-app/identity/users.json").success(function (res) {
+        $scope.users = res.users;
+        $scope.roles = res.roles;
     })
 }
