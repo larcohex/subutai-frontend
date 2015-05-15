89e1de0951722a7e455d6014327f63735e4afcb9
larik <larensolarsuss@gmail.com>
2015-05-15 10:58:16 +0600

    identity layout + lazyload code

--- subutai-app/subutai-app.js
+++ subutai-app/subutai-app.js
@@ -1,20 +1,27 @@
 var app = angular.module("subutai-app", [
     'ui.router',
+    'oc.lazyLoad',
     'ui.bootstrap',
     'mc.resizer',
     'pascalprecht.translate',
     
     'subutai.col-select',
     
-    'subutai.identity',
+    //'subutai.identity',
     'subutai.environment'
     //'app.notifyGrowl'
 ])
     .config(routesConf)
 
-routesConf.$inject = ['$stateProvider'];
+routesConf.$inject = ['$stateProvider', '$urlRouterProvider', '$ocLazyLoadProvider'];
 
-function routesConf($stateProvider) {
+function routesConf($stateProvider, $urlRouterProvider, $ocLazyLoadProvider) {
+
+    $urlRouterProvider.otherwise("/404");
+
+    $ocLazyLoadProvider.config({
+        debug: true
+    });
 
     $stateProvider
     .state(
@@ -33,15 +40,25 @@ function routesConf($stateProvider) {
         "identity", {
             url: "/identity",
             templateUrl: "subutai-app/identity/partials/view.html"
+            //resolve: {
+            //    loadPlugin: function ($ocLazyLoad) {
+            //        return $ocLazyLoad.load([
+            //            {
+            //                name: subutai.identity,
+            //                files: ['subutai-app/identity/identity.js', 'subutai-app/identity/controller.js', 'subutai-app/identity/service.js']
+            //            }
+            //        ]);
+            //    }
+            //}
         }
     )
-        .state(
+    .state(
         "metrics", {
             url: "/metrics",
             templateUrl: "subutai-app/metrics/partials/view.html"
         }
     )
-        .state(
+    .state(
         "tracker", {
             url: "/tracker",
             templateUrl: "subutai-app/tracker/partials/view.html"
@@ -51,6 +68,11 @@ function routesConf($stateProvider) {
         "environment", {
             url: "/environment",
             templateUrl: "subutai-app/environment/partials/view.html"
-            }
-    );
+        }
+    ).state(
+        "404", {
+            url: "/404",
+            template: "Not found"
+        }
+    )
 }
