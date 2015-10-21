157a1c769af3ad5982c2aa3a2e944e024cb57b54
unknown <matabaev@critical-factor.com>
2015-10-21 15:37:02 +0600

    SS-3695

--- subutai-app/tracker/controller.js
+++ subutai-app/tracker/controller.js
@@ -10,31 +10,55 @@ angular.module('subutai.tracker.controller', [])
 TrackerCtrl.$inject = ['trackerSrv', '$scope'];
 function TrackerCtrl(trackerSrv, $scope) {
 
-    $scope.startDate = new Date("2015-01-01");
-    $scope.endDate = new Date("2015-12-31");
-
     var vm = this;
     vm.loadOperations = loadOperations;
     vm.viewLogs = viewLogs;
     vm.initDataTable = initDataTable;
+
+    vm.startDate = new Date("2015-01-01");
+    vm.endDate = new Date("2015-12-31");
+    vm.logText;
 
     trackerSrv.getModules().success(function (data) {
         vm.modules = data;
     });
 
     function loadOperations() {
-        vm.operations = vm.modulesDropdown.operations;
+
+		var startDateString = vm.startDate.getFullYear() + '-' 
+			+ vm.startDate.getMonthFormatted() + '-' 
+			+ vm.startDate.getDateFormatted();
+		var endDateString = vm.endDate.getFullYear() + '-' 
+			+ vm.endDate.getMonthFormatted() + '-' 
+			+ vm.endDate.getDateFormatted();
+
+		trackerSrv.getOperations(vm.selectedModule, startDateString, endDateString, 10).success(function (data) {
+			vm.operations = data;
+			console.log(vm.operations);
+		});
     }
-    function viewLogs() {
-        for(var i = 0; i < vm.operations.length; i++) {
-            for(var j = 0; j < vm.operations[i].logs.length; j++) {
-                $('#logsContainer').append('<p>' + vm.operations[i].logs[j].date + ' ---> ' + vm.operations[i].logs[j].log + '</p>');
-            }
-        }
-    }
+
+	function viewLogs(id) {
+		$('#logsContainer').html('');
+		trackerSrv.getOperation(vm.selectedModule, id).success(function (data) {
+			vm.logText = data.log.replace(/(?:\r\n|\r|\n)/g, '<br />');;
+			$('#logsContainer').html(vm.logText);
+		});		
+	}
+
     function initDataTable() {
         $('#operationsTable').DataTable({
             responsive: true
         });
     }
+}
+
+Date.prototype.getMonthFormatted = function() {
+    var month = this.getMonth() + 1;
+    return month < 10 ? '0' + month : '' + month;
+}
+
+Date.prototype.getDateFormatted = function() {
+    var day = this.getDate();
+    return day < 10 ? '0' + day : '' + day;
 }
--- subutai-app/tracker/service.js
+++ subutai-app/tracker/service.js
@@ -10,8 +10,12 @@ trackerSrv.$inject = ['$http'];
 
 function trackerSrv($http) {
     var modulesURL = 'http://172.16.131.205:8181/cxf/tracker/operations/sources';
+    var operationsURL = 'http://172.16.131.205:8181/cxf/tracker/operations/';
+
     var trackerSrv = {
-        getModules: getModules
+        getModules: getModules,
+        getOperations: getOperations,
+        getOperation: getOperation
     };
 
     return trackerSrv;
@@ -19,5 +23,15 @@ function trackerSrv($http) {
     function getModules() {
         return $http.get(modulesURL, {withCredentials: true, headers: {'Content-Type': 'application/json'}});
     }
+
+    function getOperations(module, dateFrom, dateTo, limit) {
+		var $url = operationsURL + module + '/' + dateFrom + '/' + dateTo + '/' + limit;
+        return $http.get($url, {withCredentials: true, headers: {'Content-Type': 'application/json'}});
+    }
+
+    function getOperation(module, id) {
+		var $url = operationsURL + module + '/' + id;
+		return $http.get($url, {withCredentials: true, headers: {'Content-Type': 'application/json'}});
+    }	
 
 }
