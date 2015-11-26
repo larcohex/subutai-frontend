d46055964b0865f3aef5cdd0a5b1e85ee258ce28
unknown <matabaev@critical-factor.com>
2015-11-27 03:38:06 +0600

    SS-3951, HIPI plugin add and dinish

--- plugins/hipi/controller.js
+++ plugins/hipi/controller.js
@@ -4,13 +4,45 @@ angular.module('subutai.plugins.hipi.controller', [])
     .controller('HipiCtrl', HipiCtrl)
 	.directive('colSelectContainers', colSelectContainers);
 
-HipiCtrl.$inject = ['hipiSrv', 'SweetAlert', 'DTOptionsBuilder', 'DTColumnDefBuilder'];
+HipiCtrl.$inject = ['$scope', 'hipiSrv', 'SweetAlert', 'DTOptionsBuilder', 'DTColumnDefBuilder', 'ngDialog'];
 
-function HipiCtrl(hipiSrv, SweetAlert, DTOptionsBuilder, DTColumnDefBuilder) {
+function HipiCtrl($scope, hipiSrv, SweetAlert, DTOptionsBuilder, DTColumnDefBuilder, ngDialog) {
     var vm = this;
 	vm.activeTab = 'install';
 	vm.hipiInstall = {};
+	vm.clusters = [];
 	vm.hadoopClusters = [];
+	vm.currentClusterNodes = [];
+	vm.currentCluster = [];
+	vm.availableNodes = [];
+
+	//functions
+	vm.getClustersInfo = getClustersInfo;	
+	vm.getHadoopClusterNodes = getHadoopClusterNodes;	
+	vm.addContainer = addContainer;	
+	vm.createHipi = createHipi;	
+	vm.deleteNode = deleteNode;
+	vm.addNodeForm = addNodeForm;
+	vm.addNode = addNode;
+	vm.deleteCluster = deleteCluster;
+
+	hipiSrv.getHadoopClusters().success(function(data){
+		vm.hadoopClusters = data;
+		console.log(vm.hadoopClusters);
+		if(vm.hadoopClusters.length == 0) {
+			SweetAlert.swal("ERROR!", 'No Hadoop clusters was found! Create Hadoop cluster first.', "error");
+		}
+	}).error(function(data){
+		SweetAlert.swal("ERROR!", 'No Hadoop clusters was found! ERROR: ' + data, "error");
+	});
+	setDefaultValues();
+
+	function getClusters() {
+		hipiSrv.getClusters().success(function (data) {
+			vm.clusters = data;
+		});
+	}
+	getClusters();
 
 	vm.dtOptions = DTOptionsBuilder
 		.newOptions()
@@ -24,14 +56,149 @@ function HipiCtrl(hipiSrv, SweetAlert, DTOptionsBuilder, DTColumnDefBuilder) {
 		DTColumnDefBuilder.newColumnDef(2).notSortable()
 	];
 
-	hipiSrv.getHadoopClusters().success(function(data){
-		vm.hadoopClusters = data;
-		if(vm.hadoopClusters.length == 0) {
-			SweetAlert.swal("ERROR!", 'No Hadoop clusters was found! Create Hadoop cluster first.', "error");
+	function getClustersInfo(selectedCluster) {
+		LOADING_SCREEN();
+		hipiSrv.getClusters(selectedCluster).success(function (data) {
+			vm.currentCluster = data;
+			LOADING_SCREEN('none');
+		});
+	}
+
+	function addNodeForm() {
+		if(vm.currentCluster.clusterName === undefined) return;
+		hipiSrv.getAvailableNodes(vm.currentCluster.clusterName).success(function (data) {
+			vm.availableNodes = data;
+			console.log(vm.availableNodes);
+		});
+		ngDialog.open({
+			template: 'plugins/hipi/partials/addNodesForm.html',
+			scope: $scope
+		});
+	}
+
+	function addNode(chosenNode) {
+		if(chosenNode === undefined) return;
+		if(vm.currentCluster.clusterName === undefined) return;
+		SweetAlert.swal("Success!", "Adding node action started.", "success");
+		ngDialog.closeAll();
+		hipiSrv.addNode(vm.currentCluster.clusterName, chosenNode).success(function (data) {
+			SweetAlert.swal(
+				"Success!",
+				"Node has been added on cluster " + vm.currentCluster.clusterName + ".",
+				"success"
+			);
+			getClustersInfo(vm.currentCluster.clusterName);
+		});
+	}
+
+	function getHadoopClusterNodes(selectedCluster) {
+		LOADING_SCREEN();
+		hipiSrv.getHadoopClusters(selectedCluster).success(function (data) {
+			vm.currentClusterNodes = data.dataNodes;
+
+			var nameNodeFound = false;
+			var jobTrackerFound = false;
+			var secondaryNameNodeFound = false;
+			for(var i = 0; i < vm.currentClusterNodes.length; i++) {
+				var node = vm.currentClusterNodes[i];
+				if(node.hostname === data.nameNode.hostname) nameNodeFound = true;
+				if(node.hostname === data.jobTracker.hostname) jobTrackerFound = true;
+				if(node.hostname === data.secondaryNameNode.hostname) secondaryNameNodeFound = true;
+			}
+
+			if(!nameNodeFound) {
+				vm.currentClusterNodes.push(data.nameNode);
+			}
+			if(!jobTrackerFound) {
+				vm.currentClusterNodes.push(data.jobTracker);
+			}
+			if(!secondaryNameNodeFound) {
+				vm.currentClusterNodes.push(data.secondaryNameNode);
+			}
+
+			LOADING_SCREEN('none');
+			console.log(vm.currentClusterNodes);
+		});
+	}
+
+	function createHipi() {
+		if(vm.hipiInstall.clusterName === undefined || vm.hipiInstall.clusterName.length == 0) return;
+		if(vm.hipiInstall.hadoopClusterName === undefined || vm.hipiInstall.hadoopClusterName.length == 0) return;
+
+		SweetAlert.swal("Success!", "Hipi cluster start creating.", "success");
+		hipiSrv.createHipi(vm.hipiInstall).success(function (data) {
+			SweetAlert.swal("Success!", "Your Hipi cluster start creating.", "success");
+			getClusters();
+		}).error(function (error) {
+			SweetAlert.swal("ERROR!", 'Hipi cluster create error: ' + error, "error");
+		});
+		setDefaultValues();
+		vm.activeTab = 'manage';
+	}
+
+	function deleteCluster() {
+		if(vm.currentCluster.clusterName === undefined) return;
+		SweetAlert.swal({
+			title: "Are you sure?",
+			text: "Your will not be able to recover this cluster!",
+			type: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#ff3f3c",
+			confirmButtonText: "Delete",
+			cancelButtonText: "Cancel",
+			closeOnConfirm: false,
+			closeOnCancel: true,
+			showLoaderOnConfirm: true
+		},
+		function (isConfirm) {
+			if (isConfirm) {
+				hipiSrv.deleteCluster(vm.currentCluster.clusterName).success(function (data) {
+					SweetAlert.swal("Deleted!", "Cluster has been deleted.", "success");
+					vm.currentCluster = {};
+					getClusters();
+				}).error(function(data){
+					SweetAlert.swal("ERROR!", 'Delete cluster error: ' + data, "error");
+				});
+			}
+		});
+	}
+
+	function deleteNode(nodeId) {
+		if(vm.currentCluster.clusterName === undefined) return;
+		SweetAlert.swal({
+			title: "Are you sure?",
+			text: "Your will not be able to recover this node!",
+			type: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#ff3f3c",
+			confirmButtonText: "Delete",
+			cancelButtonText: "Cancel",
+			closeOnConfirm: false,
+			closeOnCancel: true,
+			showLoaderOnConfirm: true
+		},
+		function (isConfirm) {
+			if (isConfirm) {
+				hipiSrv.deleteNode(vm.currentCluster.clusterName, nodeId).success(function (data) {
+					SweetAlert.swal("Deleted!", "Node has been deleted.", "success");
+					vm.currentCluster = {};
+				});
+			}
+		});
+	}
+
+	function addContainer(containerId) {
+		if(vm.hipiInstall.nodes.indexOf(containerId) > -1) {
+			vm.hipiInstall.nodes.splice(vm.hipiInstall.nodes.indexOf(containerId), 1);
+		} else {
+			vm.hipiInstall.nodes.push(containerId);
 		}
-	}).error(function(data){
-		SweetAlert.swal("ERROR!", 'No Hadoop clusters was found! ERROR: ' + data, "error");
-	});
+	}
+
+	function setDefaultValues() {
+		vm.hipiInstall = {};
+		vm.hipiInstall.nodes = [];
+	}
 
 }
 
--- plugins/hipi/service.js
+++ plugins/hipi/service.js
@@ -7,14 +7,58 @@ hipiSrv.$inject = ['$http', 'hadoopSrv'];
 
 function hipiSrv($http, hadoopSrv) {
 	var BASE_URL = SERVER_URL + 'rest/hipi/';
+	var CLUSTER_URL = BASE_URL + 'clusters/';
 
 	var hipiSrv = {
-		getHadoopClusters: getHadoopClusters
+		getHadoopClusters: getHadoopClusters,
+		createHipi: createHipi,
+		getClusters: getClusters,
+		deleteNode: deleteNode,
+		getAvailableNodes: getAvailableNodes,
+		addNode: addNode,
+		deleteCluster: deleteCluster,
 	};
 
 	return hipiSrv;
 
-	function getHadoopClusters() {
-		return hadoopSrv.getClusters();
+	function addNode(clusterName, lxcHostname) {
+		return $http.post(CLUSTER_URL + clusterName + '/add/node/' + lxcHostname);
+	}
+
+	function getHadoopClusters(clusterName) {
+		return hadoopSrv.getClusters(clusterName);
+	}
+
+	function getClusters(clusterName) {
+		if(clusterName === undefined || clusterName === null) clusterName = '';
+		return $http.get(
+			CLUSTER_URL + clusterName,
+			{withCredentials: true, headers: {'Content-Type': 'application/json'}}
+		);
+	}
+
+	function getAvailableNodes(clusterName) {
+		return $http.get(
+			CLUSTER_URL + clusterName + '/available/nodes',
+			{withCredentials: true, headers: {'Content-Type': 'application/json'}}
+		);
+	}
+
+	function deleteCluster(clusterName) {
+		return $http.delete(CLUSTER_URL + 'destroy/' + clusterName);
+	}
+
+	function deleteNode(clusterName, nodeId) {
+		return $http.delete(CLUSTER_URL + clusterName + '/destroy/node/' + nodeId);
+	}
+
+	function createHipi(hipiObj) {
+		console.log(hipiObj);
+		var postData = 'clusterName=' + hipiObj.clusterName + '&hadoopClusterName=' + hipiObj.hadoopClusterName + '&nodes=' + JSON.stringify(hipiObj.nodes);
+		return $http.post(
+			CLUSTER_URL + 'install',
+			postData, 
+			{withCredentials: true, headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
+		);
 	}
 }
