03940e210798bd916d31ea9808e6066994d5c29e
unknown <matabaev@critical-factor.com>
2015-12-01 21:33:45 +0600

    SS-3971, chars fixed

--- subutai-app/monitoring/controller.js
+++ subutai-app/monitoring/controller.js
@@ -8,40 +8,55 @@ MonitoringCtrl.$inject = ['$scope', 'monitoringSrv'];
 function MonitoringCtrl($scope, monitoringSrv) {
 
 	var vm = this;
+	vm.charts = [];
 
 	//functions
-	vm.labels = [];
-	vm.series = ['Series A', 'Series B'];
-	vm.options = { pointDot: false };
-	vm.data = [[], []];
+	
 
 	monitoringSrv.getInfo().success(function (data) {
-		vm.cpu = data.metrics[0].series;
-		vm.cpu.data = {};
-		vm.cpu.data.values = [];
-		vm.cpu.data.labels = [];
-		vm.network = data.metrics[1].series;
-		vm.memory = data.metrics[2].series;
-		vm.disk = data.metrics[3].series;
-		console.log(data);
-
-		for(var j = 0; j < vm.cpu.length; j++) {
-			var currentValues = [];
-			for(var i = 0; i < vm.cpu[j].values.length; i++) {
-				var label = '';
-				if(i % 10 == 0) {
-					label = 't';
-				}
-				vm.cpu.data.labels.push(label);
-				currentValues.push(vm.cpu[j].values[i][1]);
-			}
-			vm.cpu.data.values.push(currentValues);
+		for(var i = 0; i < data.metrics.length; i++) {
+			vm.charts.push(getChartData(data.metrics[i]));
 		}
-		console.log(vm.cpu.data);
-	});	
+	});
 
 	vm.onClick = function (points, evt) {
 		console.log(points, evt);
 	};
+
+	function getChartData(obj) {
+		console.log(obj);
+		var result = {};
+		result.values = [];
+		result.labels = [];
+		result.series = [];
+		result.options = {};
+
+		var setLabels = true;
+		if(obj.series !== undefined) {
+
+			if(obj.series[0].name.indexOf('cpu') > -1) {
+				result.options = { pointDot: false, scaleShowVerticalLines: false, scaleOverride: true, scaleStartValue: 0, scaleStepWidth: 20, scaleSteps: 5, showXLabelseByEveryMinutes: 10 };
+			} else {
+				result.options = { pointDot: false, scaleShowVerticalLines: false, showXLabelseByEveryMinutes: 10 };
+			}
+
+			for(var i = 0; i < obj.series.length; i++) {
+				var currentValues = [];
+				for(var j = 0; j < obj.series[i].values.length; j++) {
+					if(setLabels) {
+						//var label = obj.series[i].values[j][0];
+						var label = moment(obj.series[i].values[j][0]).format('H:mm');
+						result.labels.push(label);
+					}
+					//var value = Math.round(obj.series[i].values[j][1]).toFixed(2);
+					currentValues.push(obj.series[i].values[j][1]);
+				}
+				setLabels = false;
+				result.values.push(currentValues);
+				result.series.push(obj.series[i].tags.type);
+			}
+		}
+		return result;
+	}
 };
 
