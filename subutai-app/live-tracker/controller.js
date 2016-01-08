b8d3110dd10fa24c5029a7b7f957798918b65cde
Manas Atabaev <matabaev@critical-factor.com>
2016-01-08 11:54:42 +0600

    live tracker resize SS-4018

--- subutai-app/live-tracker/controller.js
+++ subutai-app/live-tracker/controller.js
@@ -6,10 +6,15 @@ LiveTrackerCtrl.$inject = ['liveTrackerSrv', '$scope', '$rootScope', '$timeout']
 function LiveTrackerCtrl(liveTrackerSrv, $scope, $rootScope, $timeout) {
 
 	var vm = this;
+	vm.modules = [];
 
-	vm.selectedModule = 'ENVIRONMENT MANAGER';
+	vm.selectedModule = 'ENVIRONMENT_MANAGER';
 	vm.startDate = new Date("2015-01-01");
 	vm.endDate = new Date("2015-12-31");
+
+	liveTrackerSrv.getModules().success(function (data) {
+		vm.modules = data;
+	});
 
 }
 
