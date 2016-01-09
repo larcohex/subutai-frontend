08ab44e7c578db593120e7bc270541b09445b28c
Manas Atabaev <matabaev@critical-factor.com>
2016-01-09 11:40:22 +0600

    containers fix SS-4018

--- subutai-app/containers/controller.js
+++ subutai-app/containers/controller.js
@@ -220,9 +220,9 @@ function ContainerViewCtrl($scope, $rootScope, environmentService, SweetAlert, D
 		}
 
 		environmentService.switchContainer(vm.containers[key].id, action).success(function (data) {
-			/*environmentService.getContainerStatus(vm.containers[key].id).success(function (data) {
+			environmentService.getContainerStatus(vm.containers[key].id).success(function (data) {
 				vm.containers[key].state = data.STATE;
-			});*/
+			});
 		});		
 	}
 
