69c8a37f4292c5331a8c40a2ee77c6b35a977461
Kudaiar Pirimbaev <kpirimbaev@critical-factor.com>
2016-01-14 12:12:43 +0600

    Added About Page

--- subutai-app/about/controller.js
+++ subutai-app/about/controller.js
@@ -8,9 +8,9 @@ AboutCtrl.$inject = ['$http', 'ngDialog', 'SweetAlert', '$scope'];
 
 function AboutCtrl ($http, ngDialog, SweetAlert, $scope) {
 	var vm = this;
-	vm.text = "";
+	vm.info = {};
 	$http.get (SERVER_URL + "rest/v1/tracker/subutai/about").success (function (data) {
-		vm.text = data;
+		vm.info = data;
 		console.log (data);
 	});
 }
