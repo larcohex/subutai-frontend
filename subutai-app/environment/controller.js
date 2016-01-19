b7330cedfbebd08c7a5d4612ecf1a4c10f0189fa
Ernist Bekboev <archinstone@gmail.com>
2016-01-19 18:39:58 +0600

    add local peer to peers list

--- subutai-app/environment/controller.js
+++ subutai-app/environment/controller.js
@@ -80,6 +80,7 @@ function EnvironmentViewCtrl($scope, $rootScope, environmentService, peerRegistr
 	});
 
 	peerRegistrationService.getRequestedPeers().success(function (peers) {
+		peers.push({peerInfo: {id: 'local'}});
 		vm.peers = peers;
 	});
 
