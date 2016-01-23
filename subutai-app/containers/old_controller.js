6f41681b3e07df3bde5bc8da467f009410e91e11
Manas Atabaev <matabaev@critical-factor.com>
2016-01-23 17:49:23 +0600

    new environment cretion

---
+++ subutai-app/containers/old_controller.js
@@ -0,0 +1,229 @@
+'use strict';
+
+angular.module('subutai.containers.controller', ['ngTagsInput'])
+	.controller('ContainerViewCtrl', ContainerViewCtrl);
+
+ContainerViewCtrl.$inject = ['$scope', '$rootScope', 'environmentService', 'SweetAlert', 'DTOptionsBuilder', 'DTColumnDefBuilder', '$stateParams', 'ngDialog', '$timeout', 'cfpLoadingBar'];
+
+function ContainerViewCtrl($scope, $rootScope, environmentService, SweetAlert, DTOptionsBuilder, DTColumnDefBuilder, $stateParams, ngDialog, $timeout, cfpLoadingBar) {
+
+	var vm = this;
+
+	cfpLoadingBar.start();
+	angular.element(document).ready(function () {
+		cfpLoadingBar.complete();
+	});
+
+	vm.environments = [];
+	vm.containers = [];
+	vm.containersType = [];
+	vm.environmentId = $stateParams.environmentId;
+	vm.currentTags = [];
+	vm.allTags = [];
+	vm.tags2Container = {};
+	vm.currentDomainStatus = {};
+	vm.domainContainer = {};
+
+	// functions
+	vm.filterContainersList = filterContainersList;
+	vm.containerAction = containerAction;
+	vm.destroyContainer = destroyContainer;
+	vm.addTagForm = addTagForm;
+	vm.addTags = addTags;
+	vm.removeTag = removeTag;
+	vm.showDomainForm = showDomainForm;
+	vm.checkDomain = checkDomain;
+
+	environmentService.getContainersType().success(function (data) {
+		vm.containersType = data;
+	});
+
+	function showDomainForm(container) {
+		LOADING_SCREEN();
+		vm.currentDomainStatus = {};
+		vm.domainContainer = container;
+		environmentService.getContainerDomain(container).success(function (data) {
+			vm.currentDomainStatus = data;
+			ngDialog.open({
+				template: 'subutai-app/containers/partials/addToDomain.html',
+				scope: $scope
+			});			
+			LOADING_SCREEN('none');
+		}).error(function(error){
+			LOADING_SCREEN('none');
+			ngDialog.closeAll();
+		});
+	}
+
+	function checkDomain() {
+		environmentService.checkDomain(vm.domainContainer).success(function (data) {
+			vm.currentDomainStatus = data;
+		});
+		ngDialog.closeAll();
+	}
+
+	function addTagForm(container) {
+		vm.tags2Container = container;
+		vm.currentTags = [];
+		for(var i = 0; i < container.tags.length; i++) {
+			vm.currentTags.push({text: container.tags[i]});
+		}
+		ngDialog.open({
+			template: 'subutai-app/containers/partials/addTagForm.html',
+			scope: $scope
+		});
+	}
+
+	function addTags() {
+		var tags = [];
+		for(var i = 0; i < vm.currentTags.length; i++){
+			tags.push(vm.currentTags[i].text);
+		}
+		environmentService.setTags(vm.tags2Container.environmentId, vm.tags2Container.id, tags).success(function (data) {
+			vm.tags2Container.tags = tags;
+			console.log(data);
+		});
+		vm.tags2Container.tags = tags;
+		ngDialog.closeAll();
+	}
+
+	function removeTag(container, tag, key) {
+		environmentService.removeTag(container.environmentId, container.id, tag).success(function (data) {
+			console.log(data);
+		});
+		container.tags.splice(key, 1);
+	}
+
+	function getContainers() {
+		environmentService.getEnvironments().success(function (data) {
+
+			for(var i = 0; i < data.length; i++) {
+				data[i].containers.sort(compare);
+			}
+			data.sort(compare);
+
+			var currentArrayString = JSON.stringify(vm.environments, function( key, value ) {
+				if( key === "$$hashKey" ) {
+					return undefined;
+				}
+				return value;
+			});
+			var serverArrayString = JSON.stringify(data, function( key, value ) {
+				if( key === "$$hashKey" ) {
+					return undefined;
+				}
+				return value;
+			});
+
+			if(currentArrayString != serverArrayString) {
+				vm.environments = data;
+			}
+			filterContainersList();
+		});
+	}
+	getContainers();
+
+	function compare(a,b) {
+		if (a.id < b.id) return -1;
+		if (a.id > b.id) return 1;
+		return 0;
+	}
+
+	function filterContainersList() {
+		vm.allTags = [];
+		vm.containers = [];
+		for(var i in vm.environments) {
+			if(
+				vm.environmentId == vm.environments[i].id || 
+				vm.environmentId === undefined || 
+				vm.environmentId.length == 0
+			) {
+				for(var j in vm.environments[i].containers) {
+					if(
+						vm.containersTypeId !== undefined && 
+						vm.containersTypeId != vm.environments[i].containers[j].type && 
+						vm.containersTypeId.length > 0
+					) {continue;}
+					if(
+						vm.containerState !== undefined && 
+						vm.containerState != vm.environments[i].containers[j].state && 
+						vm.containerState.length > 0
+					) {continue;}
+					vm.containers.push(vm.environments[i].containers[j]);
+					vm.allTags = vm.allTags.concat(vm.environments[i].containers[j].tags);
+				}
+			}
+		}
+	}
+
+	vm.dtOptions = DTOptionsBuilder
+		.newOptions()
+		.withOption('order', [[ 2, "asc" ]])
+		.withOption('stateSave', true)
+		.withPaginationType('full_numbers');
+	vm.dtColumnDefs = [
+		DTColumnDefBuilder.newColumnDef(0),
+		DTColumnDefBuilder.newColumnDef(1),
+		DTColumnDefBuilder.newColumnDef(2),
+		DTColumnDefBuilder.newColumnDef(3).notSortable(),
+		DTColumnDefBuilder.newColumnDef(4).notSortable(),
+		DTColumnDefBuilder.newColumnDef(5).notSortable(),
+		DTColumnDefBuilder.newColumnDef(6).notSortable()
+	];
+
+	var refreshTable;
+	var reloadTableData = function() {
+		refreshTable = $timeout(function myFunction() {
+			getContainers();
+			refreshTable = $timeout(reloadTableData, 30000);
+		}, 30000);
+	};
+	reloadTableData();
+
+	$rootScope.$on('$stateChangeStart',	function(event, toState, toParams, fromState, fromParams){
+		console.log('cancel');
+		$timeout.cancel(refreshTable);
+	});
+
+	function destroyContainer(containerId, key) {
+		SweetAlert.swal({
+			title: "Are you sure?",
+			text: "Your will not be able to recover this Container!",
+			type: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#ff3f3c",
+			confirmButtonText: "Destroy",
+			cancelButtonText: "Cancel",
+			closeOnConfirm: false,
+			closeOnCancel: true,
+			showLoaderOnConfirm: true
+		},
+		function (isConfirm) {
+			if (isConfirm) {
+				environmentService.destroyContainer(containerId).success(function (data) {
+					SweetAlert.swal("Destroyed!", "Your container has been destroyed.", "success");
+					vm.containers.splice(key, 1);
+				}).error(function (data) {
+					SweetAlert.swal("ERROR!", "Your environment is safe :). Error: " + data.ERROR, "error");
+				});
+			}
+		});
+	}
+
+	function containerAction(key) {
+		var action = 'start';
+		if(vm.containers[key].state == 'RUNNING') {
+			action = 'stop';
+			vm.containers[key].state = 'STOPPING';
+		} else {
+			vm.containers[key].state = 'STARTING';
+		}
+
+		environmentService.switchContainer(vm.containers[key].id, action).success(function (data) {
+			environmentService.getContainerStatus(vm.containers[key].id).success(function (data) {
+				vm.containers[key].state = data.STATE;
+			});
+		});		
+	}
+
+}
--- subutai-app/environment/controller.js
+++ subutai-app/environment/controller.js
@@ -66,6 +66,7 @@ function EnvironmentViewCtrl($scope, $rootScope, environmentService, peerRegistr
 	vm.removeNodeGroup = removeNodeGroup;
 	vm.setNodeData = setNodeData;
 	vm.setupAdvancedEnvironment = setupAdvancedEnvironment;
+	vm.initJointJs = initJointJs;
 
 	environmentService.getTemplates()
 		.success(function (data) {
@@ -306,34 +307,6 @@ function EnvironmentViewCtrl($scope, $rootScope, environmentService, peerRegistr
 	});
 
 
-	/*	function createdRow(row, data, dataIndex) {
-		$compile(angular.element(row).contents())($scope);
-	}
-
-	function statusHTML(environmentStatus, type, full, meta) {
-		return '<div class="b-status-icon b-status-icon_' + environmentStatus + '" tooltips tooltip-template="' + environmentStatus + '" tooltip-side="right"></div>';
-	}
-
-	function environmentNameTooltip(data, type, full, meta) {
-		vm.users[data.id] = data;
-		return "<span tooltips tooltip-template='<span class=\"b-nowrap\">ID: <b>" + data.id + "</b></span>'>" + data.name + "</span>";
-	}
-
-	function sshKeyLinks(data, type, full, meta) {
-		var addSshKeyLink = '<a href ng-click="environmentViewCtrl.showSSHKeyForm(\'' + data.id + '\')">Add</a>';
-		var removeSshKeyLink = '<a href ng-click="environmentViewCtrl.removeSshKey(\'' + data.id + '\')">Remove</a>';
-		return addSshKeyLink + '/' + removeSshKeyLink;
-	}
-
-	function domainsTag(data, type, full, meta) {
-		return '<button class="b-btn b-btn_grey" ng-click="environmentViewCtrl.showDomainForm(\'' + data.id + '\')">Configure</button>';
-		//return '<span class="b-tags b-tags_grey" ng-click="environmentViewCtrl.removeDomain(\'' + data.id + '\')">Add <i class="fa fa-plus"></i></span>';
-	}
-
-	function actionStartEnvironmentBuild(data, type, full, meta) {
-		return '<a href="" class="b-icon b-icon_build" ng-click="environmentViewCtrl.startEnvironmentBuild(\'' + data.id + '\')"></a>';
-	}*/
-
 	function containersTags (data) {
 		var containersTotal = {};
 		for(var i = 0; i < data.containers.length; i++) {
@@ -374,10 +347,6 @@ function EnvironmentViewCtrl($scope, $rootScope, environmentService, peerRegistr
 		}
 		vm.installedContainers = containersTotal;
 	}
-
-/*	function actionDelete(data, type, full, meta) {
-		return '<a href="" class="b-icon b-icon_remove" ng-click="environmentViewCtrl.destroyEnvironment(\'' + data.id + '\')"></a>';
-	}*/
 
 	function destroyContainer(containerId) {
 		SweetAlert.swal({
@@ -454,12 +423,12 @@ function EnvironmentViewCtrl($scope, $rootScope, environmentService, peerRegistr
 			function (isConfirm) {
 				if (isConfirm) {
 					SweetAlert.swal(
-							{
-								title : 'Delete!',
-								text : 'Your environment is being deleted!!',
-								timer: VARS_TOOLTIP_TIMEOUT,
-								showConfirmButton: false
-							}
+						{
+							title : 'Delete!',
+							text : 'Your environment is being deleted!!',
+							timer: VARS_TOOLTIP_TIMEOUT,
+							showConfirmButton: false
+						}
 					);
 
 					environmentService.destroyEnvironment(environmentId).success(function (data) {
@@ -710,6 +679,176 @@ function EnvironmentViewCtrl($scope, $rootScope, environmentService, peerRegistr
 		vm.nodeList = [];
 		vm.advancedEnv = {};
 		vm.advancedEnv.currentNode = getDefaultValues();
+	}
+
+	function initJointJs() {
+		//custom shapes
+		joint.shapes.tm = {};
+
+		joint.shapes.tm.toolElement = joint.shapes.basic.Generic.extend({
+
+			toolMarkup: [
+				'<g class="element-tools">',
+					'<g class="element-tool-remove"><circle fill="red" r="11"/>',
+						'<path transform="scale(.8) translate(-16, -16)" d="M24.778,21.419 19.276,15.917 24.777,10.415 21.949,7.585 16.447,13.087 10.945,7.585 8.117,10.415 13.618,15.917 8.116,21.419 10.946,24.248 16.447,18.746 21.948,24.248z"/>',
+						'<title>Remove</title>',
+					'</g>',
+				'</g>'
+			].join(''),
+
+			defaults: joint.util.deepSupplement({
+				attrs: {
+					text: { 'font-weight': 400, 'font-size': 'small', fill: 'black', 'text-anchor': 'middle', 'ref-x': .5, 'ref-y': .5, 'y-alignment': 'middle' },
+				},
+			}, joint.shapes.basic.Generic.prototype.defaults)
+
+		});
+
+		joint.shapes.tm.devElement = joint.shapes.tm.toolElement.extend({
+
+			markup: [
+				'<g class="rotatable">',
+					'<g class="scalable">',
+						'<rect class="b-border"/>',
+					'</g>',
+					'<image/>',
+					'<rect class="b-magnet"/>',
+				'</g>'
+			].join(''),
+
+			defaults: joint.util.deepSupplement({
+				type: 'tm.devElement',
+				size: { width: 70, height: 70 },
+				attrs: {
+					'rect.b-border': {fill: '#fff', stroke: '#ccc', 'stroke-width': 1, width: 70, height: 70, rx: 5, ry: 5},
+					'rect.b-magnet': {fill: '#104eab', width: 10, height: 10, rx: 2, ry: 2, magnet: true, transform: 'translate(30,55)'},
+					image: {'ref-x': 2, 'ref-y': 2, ref: 'rect', width: 66, height: 66},
+				}
+			}, joint.shapes.tm.toolElement.prototype.defaults)
+		});
+
+		//custom view
+		joint.shapes.tm.ToolElementView = joint.dia.ElementView.extend({
+			initialize: function() {
+				joint.dia.ElementView.prototype.initialize.apply(this, arguments);
+			},
+			render: function () {
+				joint.dia.ElementView.prototype.render.apply(this, arguments);
+				this.renderTools();
+				this.update();
+				return this;
+			},
+			renderTools: function () {
+				var toolMarkup = this.model.toolMarkup || this.model.get('toolMarkup');
+				if (toolMarkup) {
+					var nodes = V(toolMarkup);
+					V(this.el).append(nodes);
+				}
+				return this;
+			},
+			pointerclick: function (evt, x, y) {
+				this._dx = x;
+				this._dy = y;
+				this._action = '';
+				var className = evt.target.parentNode.getAttribute('class');
+				switch (className) {
+					case 'element-tool-remove':
+						$('.js-add-dev-element[data-type=' + this.model.attributes.devType + ']')
+							.removeClass('b-devops-menu__li-link_active');
+						this.model.remove();
+						$('.js-devops-item-info-block').hide();
+						return;
+						break;
+					case 'rotatable':
+						console.log(this.model);
+						if(devOpsElementsInfo[this.model.attributes.devTypeId]) {
+							var itemInfo = devOpsElementsInfo[this.model.attributes.devTypeId];
+							$('.js-devops-item-info-block').hide();
+							if(itemInfo.type == 'form') {
+								$(itemInfo.text).show();
+							} else {
+								var contentBlock = $('.js-clicked-item-content');
+								var img= document.createElement('img');
+								img.src= itemInfo.img;
+								img.alt= itemInfo.title;
+								console.log(img);
+								contentBlock.find('.js-clicked-item-content__title').html(img).append('<br>' + itemInfo.title);
+								contentBlock.find('.js-clicked-item-content__text').html(itemInfo.text);
+								contentBlock.show();
+							}
+						}
+						return;
+						break;
+					default:
+				}
+				joint.dia.CellView.prototype.pointerclick.apply(this, arguments);
+			}
+		});
+		joint.shapes.tm.devElementView = joint.shapes.tm.ToolElementView;
+
+		var graph = new joint.dia.Graph;
+
+		var paper = new joint.dia.Paper({
+			el: $('#js-environment-creation'),
+			width: '100%',
+			height: '100%',
+			model: graph,
+			gridSize: 1
+		});
+
+		/*paper.on('cell:pointerdown', 
+			function(cellView, evt, x, y) { 
+				console.log(cellView);
+				console.log(graph.getCells());
+			}
+		);*/
+
+		$('#js-build-devops').on('submit', function(){
+			var allElements = graph.getCells();
+			var connections = [];
+			for(var i = 0; i < allElements.length; i++) {
+				var currentElement = allElements[i];
+				if(currentElement.attributes.type == 'link') {
+					var source = graph.getCell(currentElement.attributes.source.id);
+					var target = graph.getCell(currentElement.attributes.target.id);
+					var connection = {};
+					connection[source.attributes.devTypeId] = target.attributes.devTypeId;
+					connections.push(connection);
+				}
+			}
+			var buildObj = {};
+			$('.js-build-field').each(function(){
+				buildObj[$(this).attr('name')] = $(this).val();
+			});
+			buildObj.connections = connections;
+			console.log(buildObj);
+			return false;
+		});
+
+		var devElement = new joint.shapes.tm.devElement({
+			position: { x: 100, y: 30 },
+			size: { width: 70, height: 70 },
+			attrs: {
+				image: { 'xlink:href': 'plugins/cassandra/cassandra.png' },
+			}
+		});
+		graph.addCell(devElement);		
+
+		$('.js-add-dev-element').on('click', function(){
+			if(!$(this).hasClass('b-devops-menu__li-link_unactive') && !$(this).hasClass('b-devops-menu__li-link_active')) {
+				var devElement = new joint.shapes.tm.devElement({
+					position: { x: 100, y: 30 },
+					size: { width: 70, height: 70 },
+					devType: $(this).data('type'),
+					devTypeId: $(this).data('id'),
+					attrs: {
+						image: { 'xlink:href': $(this).data('img') },
+					}
+				});
+				graph.addCell(devElement);
+				$(this).addClass('b-devops-menu__li-link_active');
+			}
+		});
 	}
 }
 
